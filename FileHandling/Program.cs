cdc90a1603dbe7b1cc4dcf01d0522de4d0c7eb83
@programmingst <lucky.a.acidera@gmail.com>
2025-06-13 00:30:06 +0800

    Dependency Injectiton

--- FileHandling/Program.cs
+++ FileHandling/Program.cs
@@ -1,19 +1,61 @@
 ﻿namespace FileHandling
 {
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            string directoryPath = @"C:\CODES\FILES-2025-2026\C#\Text_Folders";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+            File.AppendAllText(filePath, message + "\n");
+
+        }
+    }
+
+    public class DatabaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"Logging into the Databse : {message} ");
+        }
+    }
+     
+    public class Application
+    {   
+        private readonly ILogger _ilogger;
+        public Application(ILogger logger)
+        {
+            _ilogger = logger;
+        }
+
+        public void loggerMethod(string message)
+        {
+            _ilogger.Log(message);
+
+            Console.WriteLine("Done Logging the Text...");
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            string directoryPath = @"C:\CODES\FILES-2025-2026\C#\Text_Folders";
-            string filePath = Path.Combine(directoryPath, "log.txt");
-            string message = "This is a file entry";
+            ILogger fileLogger = new FileLogger();
+            Application application = new Application(fileLogger);
+            application.loggerMethod("Sample File text");
 
-            if(!Directory.Exists(directoryPath))
-            {
-                Directory.CreateDirectory(directoryPath);
-            }
-
-            File.AppendAllText(filePath, message + "\n");
+            ILogger databaseLogger = new DatabaseLogger();
+            application = new Application(databaseLogger);
+            application.loggerMethod("Sample Database text");
         }
     }
 }
