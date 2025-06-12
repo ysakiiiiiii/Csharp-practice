222f41bc612aad5a20c28e0f317417f593444f42
@programmingst <lucky.a.acidera@gmail.com>
2025-06-13 00:07:06 +0800

    File Handling

---
+++ FileHandling/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FileHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\CODES\FILES-2025-2026\C#\Text_Folders";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+            string message = "This is a file entry";
+
+            if(!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.AppendAllText(filePath, message + "\n");
+        }
+    }
+}
--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -1,10 +1,50 @@
 ﻿namespace Polymorphism
-{
+{   
+    public interface IPaymentProcessor
+    {
+        void ProcessPayment(decimal amount);
+    }
+
+    public class CreditCardProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing Credit Card Paymount of {amount}");
+        }
+    }
+
+    public class PaypalProcessor : IPaymentProcessor
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing Paypal Paymount of {amount}");
+        }
+    }
+
+    public class PaymentService
+    {
+        private readonly IPaymentProcessor _processor;
+        public PaymentService(IPaymentProcessor processor) 
+        {
+            _processor = processor;
+        }
+
+        public void ProcessPayment(decimal amount) 
+        { 
+            _processor.ProcessPayment(amount);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IPaymentProcessor creditCarProcessor = new CreditCardProcessor();
+            PaymentService paymentService = new PaymentService(creditCarProcessor);
+            paymentService.ProcessPayment(100.00m);
+
+            IPaymentProcessor paypalProcessor = new PaypalProcessor();
+            paymentService = new PaymentService(paypalProcessor);
+            paymentService.ProcessPayment(120.00m);
         }
     }
 }
