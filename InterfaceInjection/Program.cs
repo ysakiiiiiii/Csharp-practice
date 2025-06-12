d13adb2551fc522ccd2e229e9227e4f85f88c532
@programmingst <lucky.a.acidera@gmail.com>
2025-06-13 01:04:30 +0800

    Interface Dependency Injection

---
+++ InterfaceInjection/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace InterfaceInjection
+{    public interface IWriterTools
+    {
+         void SetPaper(Paper paper);
+         void SetPencil(Pencil pencil);
+    }
+    public class Paper
+    {
+        public void Use()
+        {
+            Console.WriteLine("Preparing Paper...");
+        }
+    }
+    public class Pencil
+    {
+        public void Use()
+        {
+            Console.WriteLine("Writing in the Paper...");
+        }
+    }
+
+    public class EssayWriter : IWriterTools
+    {
+        private Pencil _pencil;
+        private Paper _paper;
+        
+        public void SetPaper(Paper paper)
+        {
+            _paper = paper;
+        }
+
+        public void SetPencil(Pencil pencil)
+        {
+            _pencil = pencil;
+        }
+
+        public void MakeEssay()
+        {
+            _paper.Use();
+            _pencil.Use();
+            Console.WriteLine("Finished Writing an Essay");
+
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Paper paper = new Paper();
+            Pencil pencil = new Pencil();
+            EssayWriter writer = new EssayWriter();
+
+            writer.SetPaper(paper);
+            writer.SetPencil(pencil);
+            writer.MakeEssay();
+        }
+    }
+}
