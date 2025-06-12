b1b596f71d2be07031139f642f48b8993fd51775
@programmingst <lucky.a.acidera@gmail.com>
2025-06-12 23:23:33 +0800

    Interface

---
+++ Interface/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Interface
+{   
+    public interface IAnimal
+    {
+        void Bark();
+    }
+
+
+    public class Dog : IAnimal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+        }
+    }
+}
