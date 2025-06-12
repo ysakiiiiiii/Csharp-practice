d09927ecef29f189f73e4ba0ebdf9dcc353de96c
@programmingst <lucky.a.acidera@gmail.com>
2025-06-12 15:46:26 +0800

    List

---
+++ List/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace List
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double  Price { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<Product> products = new List<Product>
+           {
+               new Product{Name = "Apple", Price = 3.99},
+               new Product{Name = "Bananas", Price = 1.99},
+               new Product{Name = "Berries", Price = 3.23},
+
+           };
+            products.Add(new Product { Name = "Berries", Price = 2.99 });
+
+            Console.WriteLine("Available Products");
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"\nProduct : {product.Name}\nPrice : {product.Price} ");
+            }
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <int> numbers = new List<int> {12,42,23,84};
+
+            bool hasLargeNumbers = numbers.Any(x => x > 20);
+
+            if (hasLargeNumbers)
+            {
+                Console.WriteLine("There is a large number");
+            }
+            else
+            {
+                Console.WriteLine("No large numbers in the list");
+            }
+        }
+    }
+}
