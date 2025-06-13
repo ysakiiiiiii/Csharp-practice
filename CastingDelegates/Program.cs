ccd2714c2492088f922f7c56d6e4fd1c3426f51f
@programmingst <lucky.a.acidera@gmail.com>
2025-06-13 22:51:12 +0800

    Casting Delegates

--- CastingDelegates/Program.cs
+++ CastingDelegates/Program.cs
@@ -24,6 +24,61 @@
             logHandler += logger.LogToFile;
 
             logHandler("Log this info");
+
+            foreach (LogHandler handler in logHandler.GetInvocationList())
+            {
+                try
+                {
+                    handler("Event occured with  error handling");
+                } catch (Exception ex)
+                {
+                    Console.WriteLine($"Exeption Caught : {ex.Message}");
+                }
+            }
+
+            if(IsMethodInDelegate(logHandler, logger.LogToFile))
+            {
+                logHandler -= logger.LogToFile;
+                Console.WriteLine("Log to File Remove");
+            }
+            else
+            {
+                Console.WriteLine("Log to File Method not Found");
+            }
+
+            InvokeSafely(logHandler, "After removing from the file");
+
+
         }
+
+        static void InvokeSafely(LogHandler logHandler, string message)
+        {
+            LogHandler tempLogHandler = logHandler;
+            if (tempLogHandler != null)
+            {
+                tempLogHandler(message);
+            }
+        }
+
+
+        static bool IsMethodInDelegate(LogHandler logHandler, LogHandler method)
+        {
+            if (logHandler == null)
+            {
+                return false;
+            }
+
+            foreach (var d in logHandler.GetInvocationList())   
+            {
+                if (d == (Delegate) method)
+                    {
+                        return true;
+                    }
+                
+
+            }
+
+            return false;
+        } 
     }
 }
