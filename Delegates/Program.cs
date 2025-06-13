40e68882152db1f6cf905f248b922826e68eab82
@programmingst <lucky.a.acidera@gmail.com>
2025-06-13 23:04:16 +0800

    Delegates and Events

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,15 +1,35 @@
 ﻿namespace Delegates
 {
+    public delegate void Notify(string message);
+    
+    public class EventPublisher
+    {
+        public event Notify OnNotify;
+
+        public void RaiseEvent(string message)
+        {
+            OnNotify?.Invoke(message);
+        }
+    }
+
+    public class EventSubscriber
+    {
+        public void OnEventRaised(string message)
+        {
+            Console.WriteLine($"Event Raise : {message}");
+        }
+    }
     internal class Program
     {
-        public delegate void Notify(string message);
         static void Main(string[] args)
         {
 
-            Notify notifyDelegate = ShowMessage;
-            //Notify notify = new Notify(notifyDelegate);
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subscriber = new EventSubscriber();
+            publisher.OnNotify += subscriber.OnEventRaised;
 
-            notifyDelegate("Hello Delegates");
+            publisher.RaiseEvent("test");
+
         }
 
         static void ShowMessage(string message)
